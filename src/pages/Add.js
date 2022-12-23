241ea953eb289465b86694d5e48051cb19f90037
Riley Wong <rileysw@uci.edu>
2022-12-22 23:36:35 -0800

    added pages and routes for home page and add page

---
+++ src/pages/Add.js
@@ -0,0 +1,9 @@
+function Add() {
+  return (
+      <div className="Add">
+          <h1>This is the add page.</h1>
+    </div>
+  );
+}
+
+export default Add;
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+      <div className="Home">
+          <h1>This is the home page.</h1>
+    </div>
+  );
+}
+
+export default Home;
