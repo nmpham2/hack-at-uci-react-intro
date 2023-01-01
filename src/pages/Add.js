f49bf4b9c921c50a52fd2835d40648952dd05a0d
Riley Wong <rileysw@uci.edu>
2022-12-31 23:29:32 -0800

    initial attempt at setting up Item component and Add page

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -1,7 +1,48 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function addItem(name, description) {
+  // TODO
+  console.log(name);
+  console.log(description);
+}
+
 function Add() {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const navigate = useNavigate();
+
   return (
-      <div className="Add">
-          <h1>This is the add page.</h1>
+    <div className="Add">
+      <h1>This is the add page.</h1>
+      <form
+        onSubmit={(event) => {
+          addItem(name, description);
+          event.preventDefault();
+          navigate("/home");
+        }}
+      >
+        <label>
+          Name
+          <input
+            type="text"
+            value={name}
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Description
+          <textarea
+            value={description}
+            onChange={(event) => {
+              setDescription(event.target.value);
+            }}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
